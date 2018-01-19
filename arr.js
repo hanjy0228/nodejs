775bcd4132ec5813f88e11c5ce8118c3a7276e6e
hanjy0228 <sun2013220@163.com>
2018-01-19 11:39:26 +0800

    t

---
+++ arr.js
@@ -0,0 +1,7 @@
+/**
+ * Created by HanJingYi on 2018/1/19.
+ */
+var sort=require('./arrSort');
+var a=[1,4,3];
+console.log(sort.arrSort(a));
+sort.fn();
---
+++ arrSort.js
@@ -0,0 +1,11 @@
+/**
+ * Created by HanJingYi on 2018/1/19.
+ */
+exports.arrSort=function(a){
+    return a.sort(function(a,b){
+        return a-b;
+    });
+};
+exports.fn=function(){
+    console.log(567890);
+}
